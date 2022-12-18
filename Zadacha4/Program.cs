a8de654f1900f4768b8661f484b46fbd73c55882
Vitali_Pokrovski <vitaliipokrovskii91@gmail.com>
2022-12-18 07:17:43 +0500

    Zadacha 4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 4: Напишите программу, которая принимает на входтри числа и выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("ВВедите первое число = ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("ВВедите второе число = ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.Write("ВВедите третье число = ");
+int num3 = int.Parse(Console.ReadLine());
+
+ if (num1 >= num2)
+
+ {
+    if (num3 >= num1)
+    {
+       Console.WriteLine("Максимальное равно = " + num3);
+    }
+    else
+    {
+       Console.WriteLine("Максимальное равно = " + num1);
+    }
+ }
+ else
+ {
+    if (num3 >= num2)
+    {
+       Console.WriteLine("Максимальное равно = " + num3);
+    }
+    else
+    {
+       Console.WriteLine("Максимальное равно = " + num2);
+    }
+ }
